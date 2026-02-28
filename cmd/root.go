a4273fb72c5641018ac6f5bee18437d27055ba81
p1tchbend-tool <p1tchbend.tool@gmail.com>
2026-02-28 23:44:04 +0900

    docs: rootCmdのLong descriptionのAseetを大文字に修正

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "aseet",
 	Short: "A CLI tool for various operations on Excel files",
-	Long:  `aseet is a CLI tool that allows you to perform various operations on Excel files (OOXML format). It supports commands like cat, clear, diff, grep, and sd to view, compare, search, and replace contents within Excel files.`,
+	Long:  `Aseet is a CLI tool that allows you to perform various operations on Excel files (OOXML format). It supports commands like cat, clear, diff, grep, and sd to view, compare, search, and replace contents within Excel files.`,
 }
 
 func Execute() {
